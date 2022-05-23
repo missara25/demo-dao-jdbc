f0a9c7de0a49382e877518a5ab68371b5890fd30
Sara Lima <sara.melissa.lds@gmail.com>
2022-05-23 13:42:15 -0300

    FindById implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -10,6 +10,6 @@ public interface SellerDao {
 	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
-	Department findById(Integer id);
-	List<Department>findAll();
+	Seller findById(Integer id);
+	List<Seller>findAll();
 }
